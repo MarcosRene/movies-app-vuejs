7b83c1cf80aef64511772ed8c4990758a812ee99
Marcos Renê <renemoura789@gmail.com>
2024-03-21 22:43:03 -0300

    feat: add Favorites view

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,7 +1,13 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h1>Favorites Page</h1>
-  </div>
+  <section class="py-32 px-8 flex items-center justify-center">
+    <div  class="text-center flex flex-col gap-2">
+      <h1 class="font-bold text-2xl">Lista de favoritos vazia</h1>
+      <span class="block text-base">Que tal adicionar seus filmes e sérias favoritas aqui?</span>
+      <router-link to="/" class="font-medium text-red-500 underline transform transition hover:-translate-x-2">
+        Ir para lista de filmes e séries.
+      </router-link>    
+    </div>
+  </section>
 </template>
