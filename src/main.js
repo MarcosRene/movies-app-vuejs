288d3edd02a712476c049aa85d0bc75547fb09ea
Marcos Renê <renemoura789@gmail.com>
2024-03-22 10:32:13 -0300

    chore: add vue toastfy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
+import Vue3Toastify, { toast }from 'vue3-toastify'
+
 import router from './router'
 import App from './App.vue'
-import './index.css'
 
-createApp(App).use(router).mount('#app')
+import './index.css'
+import 'vue3-toastify/dist/index.css';
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Vue3Toastify, {
+  position: toast.POSITION.BOTTOM_CENTER,
+  theme: 'colored',
+  timeout: 3000,    
+  icon: false
+})
+app.mount('#app')
